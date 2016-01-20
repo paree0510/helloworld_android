87be75360b209582f1c871f51edca75ecfae008a
pareekshith <decode2paree@gmail.com>
2016-01-20 12:16:16 +0530

    first commit

---
+++ app/src/main/java/com/example/pareekshithkj/helloworld/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.pareekshithkj.helloworld;
+
+import android.content.Intent;
+import android.provider.AlarmClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText hour, minute, msg;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        hour = (EditText) findViewById(R.id.hour);
+        minute = (EditText) findViewById(R.id.minute);
+        msg = (EditText) findViewById(R.id.msg);
+        btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent alarm = new Intent(AlarmClock.ACTION_SET_ALARM)
+                        .putExtra(AlarmClock.EXTRA_MESSAGE, msg.getText().toString())
+                        .putExtra(AlarmClock.EXTRA_HOUR, Integer.parseInt(hour.getText().toString()))
+                        .putExtra(AlarmClock.EXTRA_MINUTES, Integer.parseInt(minute.getText().toString()))
+                        .putExtra(AlarmClock.EXTRA_SKIP_UI, true);
+
+                if (alarm.resolveActivity(getPackageManager()) != null) {
+                    startActivity(alarm);
+                }
+            }
+        });
+    }
+}
