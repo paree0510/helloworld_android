d501625428628fdc74d28681017634e2ba18e7cb
pareekshith <decode2paree@gmail.com>
2016-01-21 14:53:00 +0530

    second commit

--- app/src/main/java/com/example/pareekshithkj/helloworld/MainActivity.java
+++ app/src/main/java/com/example/pareekshithkj/helloworld/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     EditText hour, minute, msg;
     Button btn;
